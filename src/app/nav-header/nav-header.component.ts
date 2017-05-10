cbb0e7d740b50a58c571dcbb53390e0902cf403b
jtaaa <joshuatallum@gmail.com>
2017-05-10 14:40:32 -0400

    Add app nav-header

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -7,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavHeaderComponent implements OnInit {
 
-  constructor() { }
+  links: Object[];
+
+  menuActive: boolean;
+
+  constructor() {
+    this.links = [
+      {
+        name: 'Home',
+        link: '/home'
+      },
+      {
+        name: 'Exec-Team',
+        link: '/execs'
+      },
+      {
+        name: 'Members',
+        link: '/members'
+      },
+      {
+        name: 'Calendar',
+        link: '/calendar'
+      }
+    ];
+    this.menuActive = false;
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+    console.log(this.menuActive);
+  }
 
   ngOnInit() {
   }
