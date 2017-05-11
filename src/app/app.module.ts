9a04db049fa204c7f3661caf3be20c12b3e76c20
jtaaa <joshuatallum@gmail.com>
2017-05-11 11:41:48 -0400

    Switch to Angular 2 animations for navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule }   from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
@@ -16,6 +18,7 @@ import { NavHeaderComponent } from './nav-header/nav-header.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -1,9 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'nav-header',
   templateUrl: './nav-header.component.html',
-  styleUrls: ['./nav-header.component.scss']
+  styleUrls: ['./nav-header.component.scss'],
+  animations: [
+    trigger('fade-in', [
+      state('in', style({opacity: 0.98})),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(150)
+      ]),
+      transition('* => void', [
+        animate(150, style({opacity: 0}))
+      ])
+    ]),
+    trigger('shrink-in', [
+      state('in', style({transform: 'scale(1, 1)'})),
+      transition('void => *', [
+        style({transform: 'scale(1.2, 2)'}),
+        animate(150)
+      ]),
+      transition('* => void', [
+        animate(150, style({transform: 'scale(1.2, 2)'}))
+      ])
+    ])
+  ]
 })
 export class NavHeaderComponent implements OnInit {
 
