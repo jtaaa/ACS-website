9bd9a422140c47c922c816a15302753d5f3f98d1
jtaaa <joshuatallum@gmail.com>
2017-05-12 12:25:59 -0400

    Make slideshow images scrollable

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+let IMG_PATH = '../../assets/images/slideshow/'
+
 @Component({
   selector: 'slideshow',
   templateUrl: './slideshow.component.html',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideshowComponent implements OnInit {
 
-  constructor() { }
+  images: Object[];
+
+  curPos: number = 3;
+
+  constructor() {
+
+    this.images = [
+      {
+        src: IMG_PATH + 'shankjoshdylan.jpg',
+        pos: 1
+      },
+      {
+        src: IMG_PATH + 'shots.jpg',
+        pos: 2
+      },
+      {
+        src: IMG_PATH + 'adam.jpg',
+        pos: 3
+      },
+      {
+        src: IMG_PATH + 'kern.jpg',
+        pos: 4
+      }
+    ]
+  }
 
   ngOnInit() {
+  }
+
+  scrollLeft() {
+    if (this.curPos > 1) {
+      this.curPos -= 1;
+    }
+    else {
+      console.log('No where to go left!')
+    }
+  }
+  scrollRight() {
+    if (this.curPos < this.images.length) {
+      this.curPos += 1;
+    }
+    else {
+      console.log('No where to go right!')
+    }
   }
 
 }
