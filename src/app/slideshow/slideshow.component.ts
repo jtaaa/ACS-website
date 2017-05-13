fae9fd6e0f6e45a5489d1b86fceec4bd69421042
jtaaa <joshuatallum@gmail.com>
2017-05-13 12:43:46 -0400

    Add slideshow scroll animations

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 let IMG_PATH = '../../assets/images/slideshow/'
 
 @Component({
   selector: 'slideshow',
   templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.scss']
+  styleUrls: ['./slideshow.component.scss'],
+  animations: [
+    trigger('scroll-slideshow', [
+      state('left',   style({ transform: 'translateX(-100%)' })),
+      state('right',  style({ transform: 'translateX(100%)'  })),
+      state('center', style({ transform: 'translateX(0)'     })),
+      transition('left => center', [
+        animate('0.2s 50ms ease-out')
+      ]),
+      transition('center => left', [
+        animate('0.2s 50ms ease-out')
+      ]),
+      transition('center => right', [
+        animate('0.2s 50ms ease-out')
+      ]),
+      transition('right => center', [
+        animate('0.2s 50ms ease-out')
+      ])
+    ])
+  ]
 })
 export class SlideshowComponent implements OnInit {
 
-  images: Object[];
+  images: any[];
 
   curPos: number = 4;
 
@@ -18,31 +44,38 @@ export class SlideshowComponent implements OnInit {
     this.images = [
       {
         src: IMG_PATH + 'shankjoshdylan.jpg',
-        pos: 1
+        pos: 1,
+        state: 'left'
       },
       {
         src: IMG_PATH + 'shots.jpg',
-        pos: 2
+        pos: 2,
+        state: 'left'
       },
       {
         src: IMG_PATH + 'adam.jpg',
-        pos: 3
+        pos: 3,
+        state: 'left'
       },
       {
         src: IMG_PATH + 'kern.jpg',
-        pos: 4
+        pos: 4,
+        state: 'center'
       },
       {
         src: IMG_PATH + 'me.jpg',
-        pos: 5
+        pos: 5,
+        state: 'right'
       },
       {
         src: IMG_PATH + 'adam2.jpg',
-        pos: 6
+        pos: 6,
+        state: 'right'
       },
       {
         src: IMG_PATH + 'tim.jpg',
-        pos: 7
+        pos: 7,
+        state: 'right'
       }
     ]
   }
@@ -53,6 +86,7 @@ export class SlideshowComponent implements OnInit {
   scrollLeft() {
     if (this.curPos > 1) {
       this.curPos -= 1;
+      this.updateCenter();
     }
     else {
       console.log('No where to go left!')
@@ -61,6 +95,7 @@ export class SlideshowComponent implements OnInit {
   scrollRight() {
     if (this.curPos < this.images.length) {
       this.curPos += 1;
+      this.updateCenter();
     }
     else {
       console.log('No where to go right!')
@@ -68,6 +103,19 @@ export class SlideshowComponent implements OnInit {
   }
   gotoPos(pos) {
     this.curPos = pos;
+    this.updateCenter();
+  }
+
+  updateCenter() {
+    for (let image of this.images) {
+      if (image.pos > this.curPos) {
+        image.state = 'right';
+      } else if (image.pos < this.curPos) {
+        image.state = 'left'
+      } else {
+        image.state = 'center';
+      }
+    }
   }
 
 }
