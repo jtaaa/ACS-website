3de0b119616dea2e561720e5dc20ca6ac5b224be
jtaaa <joshuatallum@gmail.com>
2017-05-14 14:56:04 -0400

    Add event images

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-let IMG_PATH = '../../assets/images/slideshow/'
+let IMG_PATH = '../../assets/images/'
 
 @Component({
   selector: 'app-main',
@@ -16,37 +16,37 @@ export class MainComponent implements OnInit {
 
     this.peopleImages = [
       {
-        src: IMG_PATH + 'shankjoshdylan.jpg',
+        src: IMG_PATH + 'slideshow/shankjoshdylan.jpg',
         pos: 1,
         state: 'left'
       },
       {
-        src: IMG_PATH + 'shots.jpg',
+        src: IMG_PATH + 'slideshow/shots.jpg',
         pos: 2,
         state: 'left'
       },
       {
-        src: IMG_PATH + 'adam.jpg',
+        src: IMG_PATH + 'slideshow/adam.jpg',
         pos: 3,
         state: 'left'
       },
       {
-        src: IMG_PATH + 'kern.jpg',
+        src: IMG_PATH + 'slideshow/kern.jpg',
         pos: 4,
         state: 'center'
       },
       {
-        src: IMG_PATH + 'me.jpg',
+        src: IMG_PATH + 'slideshow/me.jpg',
         pos: 5,
         state: 'right'
       },
       {
-        src: IMG_PATH + 'adam2.jpg',
+        src: IMG_PATH + 'slideshow/adam2.jpg',
         pos: 6,
         state: 'right'
       },
       {
-        src: IMG_PATH + 'tim.jpg',
+        src: IMG_PATH + 'slideshow/tim.jpg',
         pos: 7,
         state: 'right'
       }
@@ -54,12 +54,12 @@ export class MainComponent implements OnInit {
 
     this.eventImages = [
       {
-        src: IMG_PATH + 'adam2.jpg',
+        src: IMG_PATH + 'events/GBM.jpg',
         pos: 1,
         state: 'center'
       },
       {
-        src: IMG_PATH + 'tim.jpg',
+        src: IMG_PATH + 'events/fallforthecaribbean.jpg',
         pos: 2,
         state: 'right'
       }
