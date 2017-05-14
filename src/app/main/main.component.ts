7830f34231b77b2237301b297da92c2b4eb8d7e4
jtaaa <joshuatallum@gmail.com>
2017-05-14 10:39:49 -0400

    Move slideshow images object to main and bind to child slideshow component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+let IMG_PATH = '../../assets/images/slideshow/'
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,7 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  peopleImages: any[];
+  eventImages:  any[];
+
+  constructor() {
+
+    this.peopleImages = [
+      {
+        src: IMG_PATH + 'shankjoshdylan.jpg',
+        pos: 1,
+        state: 'left'
+      },
+      {
+        src: IMG_PATH + 'shots.jpg',
+        pos: 2,
+        state: 'left'
+      },
+      {
+        src: IMG_PATH + 'adam.jpg',
+        pos: 3,
+        state: 'left'
+      },
+      {
+        src: IMG_PATH + 'kern.jpg',
+        pos: 4,
+        state: 'center'
+      },
+      {
+        src: IMG_PATH + 'me.jpg',
+        pos: 5,
+        state: 'right'
+      },
+      {
+        src: IMG_PATH + 'adam2.jpg',
+        pos: 6,
+        state: 'right'
+      },
+      {
+        src: IMG_PATH + 'tim.jpg',
+        pos: 7,
+        state: 'right'
+      }
+    ]
+
+    this.eventImages = [
+      {
+        src: IMG_PATH + 'adam2.jpg',
+        pos: 1,
+        state: 'center'
+      },
+      {
+        src: IMG_PATH + 'tim.jpg',
+        pos: 2,
+        state: 'right'
+      }
+    ]
+  }
 
   ngOnInit() {
   }
--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
   trigger,
   state,
@@ -6,8 +6,6 @@ import {
   animate,
   transition
 } from '@angular/animations';
-
-let IMG_PATH = '../../assets/images/slideshow/'
 
 @Component({
   selector: 'slideshow',
@@ -35,49 +33,11 @@ let IMG_PATH = '../../assets/images/slideshow/'
 })
 export class SlideshowComponent implements OnInit {
 
-  images: any[];
+  @Input() images: any[];
 
-  curPos: number = 4;
+  @Input() curPos: number;
 
   constructor() {
-
-    this.images = [
-      {
-        src: IMG_PATH + 'shankjoshdylan.jpg',
-        pos: 1,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'shots.jpg',
-        pos: 2,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'adam.jpg',
-        pos: 3,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'kern.jpg',
-        pos: 4,
-        state: 'center'
-      },
-      {
-        src: IMG_PATH + 'me.jpg',
-        pos: 5,
-        state: 'right'
-      },
-      {
-        src: IMG_PATH + 'adam2.jpg',
-        pos: 6,
-        state: 'right'
-      },
-      {
-        src: IMG_PATH + 'tim.jpg',
-        pos: 7,
-        state: 'right'
-      }
-    ]
   }
 
   ngOnInit() {
