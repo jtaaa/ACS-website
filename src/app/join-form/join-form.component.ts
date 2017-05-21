14527e12db0202bb5dcbccaf91cfab9ed22b4fb7
jtaaa <joshuatallum@gmail.com>
2017-05-21 12:28:30 -0400

    Update join form to push to firebase database

--- src/app/join-form/join-form.component.ts
+++ src/app/join-form/join-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { MemberData } from '../join-page/join-page.component'
 
 @Component({
@@ -9,14 +10,45 @@ import { MemberData } from '../join-page/join-page.component'
 export class JoinFormComponent implements OnInit {
 
   model = new MemberData('', '', '', '');
+  submitted: boolean = false;
+  editting: boolean = false;
+  deleted: boolean = false;
+  prevStudentNumber: string = '';
 
-  submitted = false;
+  memberList: FirebaseListObservable<any[]>;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit(form) {
+    this.submitted = true;
+    if (this.editting && this.prevStudentNumber &&
+        this.prevStudentNumber != this.model.studentNumber) {
+      this.remove(this.prevStudentNumber);
+      this.editting = false;
+    }
+    const toSend = this.db.object(`/members/${this.model.studentNumber}`);
+    toSend.set(this.model);
+  }
+  onEdit(editModel) {
+    this.submitted = false;
+    this.editting = true;
+    this.prevStudentNumber = editModel.studentNumber;
+  }
+  remove(studentNumber) {
+    const toDelete = this.db.object(`/members/${studentNumber}`);
+    toDelete.remove();
+  }
+  delete(studentNumber) {
+    this.remove(studentNumber);
+    this.model = new MemberData('', '', '', '');
+    this.deleted = true;
+    this.editting = false;
+    this.submitted = false;
+  }
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.memberList = db.list('/members');
+  }
 
   ngOnInit() {
   }
-  
+
 }
