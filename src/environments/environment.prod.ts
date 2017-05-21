65ed4b1ec873cbaa79a40b94779eea385a08fce1
jtaaa <joshuatallum@gmail.com>
2017-05-21 13:35:13 -0400

    Add firebase config object

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChO-pANcs3NWnIK7fIN1X2RgQbq-x_QoA",
+    authDomain: "acs-website-be108.firebaseapp.com",
+    databaseURL: "https://acs-website-be108.firebaseio.com",
+    projectId: "acs-website-be108",
+    storageBucket: "acs-website-be108.appspot.com",
+    messagingSenderId: "946942397190"
+  }
 };
