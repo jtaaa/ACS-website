085d2e2469985a7c4b8786543f64494b49a0c2e1
jtaaa <joshuatallum@gmail.com>
2017-05-25 23:14:23 -0400

    Switch to using EventService

--- src/app/calendar-page/calendar-page.component.ts
+++ src/app/calendar-page/calendar-page.component.ts
@@ -1,107 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ACSEvent } from '../events/ACSEvent';
+import { EventService } from '../events/event.service';
+
 let IMG_PATH = './assets/images/'
 
 @Component({
   selector: 'calendar-page',
   templateUrl: './calendar-page.component.html',
-  styleUrls: ['./calendar-page.component.scss']
+  styleUrls: ['./calendar-page.component.scss'],
+  providers: [EventService]
 })
 export class CalendarPageComponent implements OnInit {
 
-  events: any[];
-  pastEvents: any[];
+  events: ACSEvent[];
+  pastEvents: ACSEvent[];
 
-  constructor() {
-    this.events = [
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/GBM/GBM.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/GBM/GBM_people.jpg',
-            pos: 2,
-            state: 'right'
-          }
-        ],
-        title: "Our first General Body Meeting for the Spring Term 2017!",
-        description: `Come out and meet us. You get to meet all the new exec and we
-                      will be voting in some new exec members for this Spring term.`
-      }
-    ];
-    this.pastEvents = [
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean-me.jpg',
-            pos: 2,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean-dari.jpg',
-            pos: 3,
-            state: 'right'
-          }
-        ],
-        title: "Back to the Caribbean! For one night only.",
-        description: `Its that time again! Join ACS as we heat up The Bombshelter Pub on the 10th of February for Back to the Caribbean!`
-      },
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/fallforthecaribbean/fallforthecaribbean.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/fallforthecaribbean/fallforthecaribbean_portrait.jpg',
-            pos: 2,
-            state: 'right'
-          }
-        ],
-        title: "Our annual Fall formal Dinner, Fall 2017!",
-        description: `It is once again time for ACS's annual fall semi-formal event, Fall For the Caribbean.
-                      Join us for some good company, beautiful displays of costume and music and of course, food!`
-      },
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-flags.jpg',
-            pos: 2,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-shank.jpg',
-            pos: 3,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-people.jpg',
-            pos: 4,
-            state: 'right'
-          }
-        ],
-        title: "Caribbean Pulse - The Heartbeat of the Islands",
-        description: `Each semester ACS sells out Bomber and this Fall it is going to be bigger and better with give aways and great music!`
-      }
-    ]
+  constructor(private eventService: EventService) {
+  }
+
+  getEvents(): void {
+    this.eventService.getEvents().then(events => this.events = events);
+  }
+
+  getPastEvents(): void {
+    this.eventService.getPastEvents().then(events => this.pastEvents = events);
   }
 
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getEvents();
+    this.getPastEvents();
   }
 
   toFloat(s) {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ACSEvent } from '../events/ACSEvent';
+import { EventService } from '../events/event.service';
+
 let IMG_PATH = './assets/images/'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  providers: [EventService]
 })
 export class MainComponent implements OnInit {
 
   peopleImages: any[];
-  events: any[];
-  pastEvents: any[];
+  events: ACSEvent[];
+  pastEvents: ACSEvent[];
 
-  constructor() {
-
+  constructor(private eventService: EventService) {
     this.peopleImages = [
       {
         src: IMG_PATH + 'slideshow/shankjoshdylan.jpg',
@@ -52,96 +55,19 @@ export class MainComponent implements OnInit {
         state: 'right'
       }
     ];
-
-    this.events = [
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/GBM/GBM.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/GBM/GBM_people.jpg',
-            pos: 2,
-            state: 'right'
-          }
-        ],
-        title: "Our first General Body Meeting for the Spring Term 2017!",
-        description: `Come out and meet us. You get to meet all the new exec and we
-                      will be voting in some new exec members for this Spring term.`
-      }
-    ];
-
-    this.pastEvents = [
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean-me.jpg',
-            pos: 2,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/backtothecaribbean/backtothecaribbean-dari.jpg',
-            pos: 3,
-            state: 'right'
-          }
-        ],
-        title: "Back to the Caribbean! For one night only.",
-        description: `Its that time again! Join ACS as we heat up The Bombshelter Pub on the 10th of February for Back to the Caribbean!`
-      },
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/fallforthecaribbean/fallforthecaribbean.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/fallforthecaribbean/fallforthecaribbean_portrait.jpg',
-            pos: 2,
-            state: 'right'
-          }
-        ],
-        title: "Our annual Fall formal Dinner, Fall 2017!",
-        description: `It is once again time for ACS's annual fall semi-formal event, Fall For the Caribbean.
-                      Join us for some good company, beautiful displays of costume and music and of course, food!`
-      },
-      {
-        images: [
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse.jpg',
-            pos: 1,
-            state: 'center'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-flags.jpg',
-            pos: 2,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-shank.jpg',
-            pos: 3,
-            state: 'right'
-          },
-          {
-            src: IMG_PATH + 'events/caribbeanpulse/caribbeanpulse-people.jpg',
-            pos: 4,
-            state: 'right'
-          }
-        ],
-        title: "Caribbean Pulse - The Heartbeat of the Islands",
-        description: `Each semester ACS sells out Bomber and this Fall it is going to be bigger and better with give aways and great music!`
-      }
-    ]
   }
 
-  ngOnInit() {
+  getEvents(): void {
+    this.eventService.getEvents().then(events => this.events = events);
+  }
+
+  getPastEvents(): void {
+    this.eventService.getPastEvents().then(events => this.pastEvents = events);
+  }
+
+  ngOnInit(): void {
+    this.getEvents();
+    this.getPastEvents();
   }
 
   toFloat(s) {
