a6c67108d64f4306ae28d484aaac628441e6caa8
jtaaa <joshuatallum@gmail.com>
2017-05-27 14:01:42 -0400

    Migrate peopleImages to db

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { ACSEvent } from '../events/ACSEvent';
 import { EventService } from '../events/event.service';
 
-let IMG_PATH = './assets/images/'
 
 @Component({
   selector: 'app-main',
@@ -13,48 +12,11 @@ let IMG_PATH = './assets/images/'
 })
 export class MainComponent implements OnInit {
 
-  peopleImages: any[];
-  events: ACSEvent[];
-  pastEvents: ACSEvent[];
+  peopleImagesDBPath: string = 'peopleImages';
+  events: ACSEvent[] = [];
+  pastEvents: ACSEvent[] = [];
 
   constructor(private eventService: EventService) {
-    this.peopleImages = [
-      {
-        src: IMG_PATH + 'slideshow/shankjoshdylan.jpg',
-        pos: 1,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'slideshow/shots.jpg',
-        pos: 2,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'slideshow/adam.jpg',
-        pos: 3,
-        state: 'left'
-      },
-      {
-        src: IMG_PATH + 'slideshow/kern.jpg',
-        pos: 4,
-        state: 'center'
-      },
-      {
-        src: IMG_PATH + 'slideshow/me.jpg',
-        pos: 5,
-        state: 'right'
-      },
-      {
-        src: IMG_PATH + 'slideshow/adam2.jpg',
-        pos: 6,
-        state: 'right'
-      },
-      {
-        src: IMG_PATH + 'slideshow/tim.jpg',
-        pos: 7,
-        state: 'right'
-      }
-    ];
   }
 
   getEvents(): void {
